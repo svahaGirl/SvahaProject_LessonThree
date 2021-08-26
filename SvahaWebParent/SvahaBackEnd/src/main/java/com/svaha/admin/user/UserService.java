8b391298f9253888f922473234634925e3ea9e77
svahaGirl <fyanton@hotmail.com>
2021-08-25 20:32:56 -0700

    Initial commit

---
+++ SvahaWebParent/SvahaBackEnd/src/main/java/com/svaha/admin/user/UserService.java
@@ -0,0 +1,53 @@
+package com.svaha.admin.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.svaha.common.entity.Role;
+import com.svaha.common.entity.User;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Autowired
+	private RoleRepository roleRepo;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+
+	public List<User> listAll() {
+		return (List<User>) userRepo.findAll();
+
+	}
+
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepo.findAll();
+
+	}
+
+	public void save(User user) {
+		encodePassword(user);
+		userRepo.save(user);
+
+	}
+
+	private void encodePassword(User user) {
+		
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+	}
+	
+	public boolean isEmailUnique(String email) {
+		
+		User userByEmail = userRepo.getUserByEmail(email);
+		return userByEmail == null;
+	}
+	
+}
